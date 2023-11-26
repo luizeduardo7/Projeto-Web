beab98499ca31adf1440d10acf146fd2d7df2a50
luizeduardo7 <luisedu11201184@gmail.com>
2023-11-25 21:36:02 -0300

    Front Usuarios login/cadastro INC

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,37 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
 
 def login(request):
-    return render(request, 'login.html')
-
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        user = authenticate(username=username, password=senha)
+    if user:
+        return HttpResponseRedirect('../admin')
+    else:
+        return HttpResponseRedirect('login')
+    
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        email = request.POST.get('email')
+        telefone = request.POST.get('telefone')
+        
+        user = User.objects.filter(username=username).first()
+        if user:
+            return HttpResponseRedirect('cadastro')
+        
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+        
+        return HttpResponseRedirect('login')
+            
+        
